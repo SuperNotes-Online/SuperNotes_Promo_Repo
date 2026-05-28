d524b4832f94d1e946034d080aba6e7ed42c2209
jazziejoy7 <jcdangdesigns@gmail.com>
2026-05-28 02:45:41 -0400

    Cleanup: remove temp scripts, graphify output, and leftover files

--- parse2.py
+++
@@ -1,9 +0,0 @@
-import sys
-with open('SuperNotes.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-idx = text.find('class="sidebar"')
-if idx != -1:
-    print(text[idx:idx+1500])
-else:
-    print("Not found")
